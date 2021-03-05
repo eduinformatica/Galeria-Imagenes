aca1e0492f86f4c45a848b0f60d9959ebd45b3ad
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-03-05 00:56:50 -0300

    se ingreso code php

--- login.php
+++ login.php
@@ -25,3 +25,16 @@
 	</div>
 </body>
 </html>
+<?php
+$link      = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+$thread_id = mysql_thread_id($link);
+if ($thread_id) {
+    printf("current thread id is %d\n", $thread_id);
+}
+
+$link = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+printf("MySQL host info: %s\n", mysql_get_host_info());
+?>
