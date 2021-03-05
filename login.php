efbcc4b34e6ebd861c33e3e0f14740686bb7afa7
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-03-05 00:32:11 -0300

    no hubo cambios importantes

--- login.php
+++ login.php
@@ -4,8 +4,8 @@
 	<meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>ImagesPro - Login</title>
-	<link rel="shortcut icon" href="./assets/img/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="./assets/css/estilos.css">
+	<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="assets/css/estilos.css">
 </head>
 <body style="font-family: 'Lato', sans-serif;">
 	<div class="login__contenedor">
