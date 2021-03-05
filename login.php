3fc831e272243cd0c9910eb7baab44dd7dc57ead
Daniel_Gómez_G <dgomez.informatica@gmail.com>
2021-03-05 04:11:25 -0300

    no hubo cambios importantes

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>ImagesPro - Login</title>
+	<title>ImagesPro's - Login</title>
 	<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="assets/css/estilos.css">
 </head>
